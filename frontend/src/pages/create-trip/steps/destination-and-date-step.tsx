5a46469638cda7d67e5dbfa8858a172b6ebea8ea
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2024-07-11 20:00:06 -0300

    fix: improve datepicker display

--- frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
+++ frontend/src/pages/create-trip/steps/destination-and-date-step.tsx
@@ -54,7 +54,7 @@ export function DestinationAndDateStep ({ isGuestInputOpen, handleCloseGuestInpu
 
       { isDatePickerOpen && (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-          <div className='w-[320px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+          <div className='w-[360px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
             <div className='space-y-2'>
               <div className='flex items-center justify-between'>
                 <h2 className='text-lg font-semibold'>Selecione a data</h2>
